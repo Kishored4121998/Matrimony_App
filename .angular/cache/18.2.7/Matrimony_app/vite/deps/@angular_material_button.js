61c69fc20f7b7402154ac53c0a0e6dcc40d8e64f
Kishore D <Kishore.d4121998@gmail.com>
2024-10-06 17:11:36 +0530

    Initial commit

---
+++ .angular/cache/18.2.7/Matrimony_app/vite/deps/@angular_material_button.js
@@ -0,0 +1,33 @@
+import {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+} from "./chunk-WGWSAON3.js";
+import "./chunk-QNPVRPN3.js";
+import "./chunk-WMHJXSWI.js";
+import "./chunk-ERHBEEMJ.js";
+import "./chunk-3OV72XIM.js";
+export {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+};
+//# sourceMappingURL=@angular_material_button.js.map
